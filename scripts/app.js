d49c1a4e78d5e48bd5071412d23b0ad9fe8045d0
Jashan Rai <jashanrai98@gmail.com>
2020-11-15 22:14:44 -0800

    changed main page to login page; changed up entire login page layout under the index.html file; need to figure out login verification still.

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,4 @@
-const studentList = document.querySelector('#studentList');
-const studentInfoForm = document.querySelector('#add-student-info');
 
-//create element and render student list
-function renderStudent(doc){
-    let li = document.createElement('li');
-    let name = document.createElement('span');
-    let set = document.createElement('span');
-
-    li.setAttribute('data-id', doc.id);
-    name.textContent = doc.data().STU_NAME;
-    set.textContent = doc.data().STU_SET;
-
-    li.appendChild(name);
-    li.appendChild(set);
-
-    studentList.appendChild(li);
-}
-
-//getting data
-db.collection("student").get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        renderStudent(doc);
-    });
-})
 
 //saving student information
 studentInfoForm.addEventListener('submit', (e) =>{
