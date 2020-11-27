5717b1133ceb53785f873d7770109cc64887fd91
Sheng Qin <qinsheng32@gmail.com>
2020-11-27 01:37:13 -0800

    do something to read data from firebase

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,165 +1,165 @@
     var date = "default";
-        var course = "default"; //month to course
-        var taskname = "default";
-        var details = "default";
-        var tasktype = "default";
-        var i = 0;
-        // var database = firebase.database();
-        // var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
-        // starCountRef.on('value', function (snapshot) {
-        //     updateStarCount(postElement, snapshot.val());
-        // });
-        $(document).ready(function () {
+    var course = "default"; //month to course
+    var taskname = "default";
+    var details = "default";
+    var tasktype = "default";
+    var i = 0;
+    // var database = firebase.database();
+    // var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
+    // starCountRef.on('value', function (snapshot) {
+    //     updateStarCount(postElement, snapshot.val());
+    // });
+    $(document).ready(function () {
 
-            console.log($("span.col-1").text());
-            //read tasks data from firebase
+        console.log($("span.col-1").text());
+        //read tasks data from firebase
 
-            // read input from textfield
-            $("select.course").change(function () {
-                course = $(this).children().children("option:selected").val();
-                console.log(month);
-            });
-
-
-            $("select.date").change(function () {
-                date = $(this).children().children("option:selected").val();
-                console.log(date);
-
-            });
-
-            $("select.tasktype").change(function () {
-                tasktype = $(this).children().children("option:selected").val();
-                console.log(tasktype);
-            });
-
-            $("input.taskname").change(function () {
-                taskname = $(this).val();
-                console.log(taskname);
-            });
+        // read input from textfield
+        $("select.course").change(function () {
+            course = $(this).children().children("option:selected").val();
+            console.log(month);
+        });
 
 
-            $("input.details").change(function () {
-                details = $(this).val();
-                console.log(details);
-            });
+        $("select.date").change(function () {
+            date = $(this).children().children("option:selected").val();
+            console.log(date);
 
-            console.log("Hello world!");
+        });
 
-// //const taskList = document.querySelector('');
-// const taskForm = document.querySelector('#taskForm')
+        $("select.tasktype").change(function () {
+            tasktype = $(this).children().children("option:selected").val();
+            console.log(tasktype);
+        });
 
-// //create element and render student list
-// function renderCourse(doc){
-//     let li = document.createElement('li');
-//     let crsName = document.createElement('div');
-//     //let crsDetail = document.createElement('div');
-//     let taskType = document.createElement('div');
-//     let dueDate = document.createElement('div');
-//     let taskDetails= document.createElement('div');
+        $("input.taskname").change(function () {
+            taskname = $(this).val();
+            console.log(taskname);
+        });
 
-//     li.setAttribute('data-id', doc.id);
-//     crsName.textContent = doc.data().CRS_NAME;
-//     taskType.textContent = doc.data().TASK_TYPE;
-//     dueDate.textContent = doc.data().DUE_DATE;
-//     taskDetails.textContent = doc.data().TASK_DETAILS;
 
-//     li.appendChild(crsName);
-//     li.appendChild(taskType);
-//     li.appendChild(dueDate);
-//     li.appendChild(taskDetails);
+        $("input.details").change(function () {
+            details = $(this).val();
+            console.log(details);
+        });
 
-//     taskForm.appendChild(li);
-// }
+        console.log("Hello world!");
 
-// //saving student information
-// form.addEventListener('submit', (e) =>{
-//     //e.preventDefault();
-//     db.collection('assignments').add({
-//         CRS_NAME: taskForm.crsName.value,
-//         TASK_TYPE: taskForm.taskType.value,
-//         DUE_DATE: taskForm.dueDate.value,
-//         TASK_DETAILS: taskForm.taskDetails.value,
-//     });
-//     taskForm.crsName.value = '';
-//     taskForm.taskType.value = '';
-//     taskForm.dueDate.value = '';
-//     taskForm.taskDatails.value = '';
- });
-/*
-            //add button and add stuff from textfield
-             $('#add').click(function () {
-                 console.log("test");
+        // //const taskList = document.querySelector('');
+        // const taskForm = document.querySelector('#taskForm')
 
-                 // Get the button that opens the modal
-                 var btn = document.getElementById("add");
+        // //create element and render student list
+        // function renderCourse(doc){
+        //     let li = document.createElement('li');
+        //     let crsName = document.createElement('div');
+        //     //let crsDetail = document.createElement('div');
+        //     let taskType = document.createElement('div');
+        //     let dueDate = document.createElement('div');
+        //     let taskDetails= document.createElement('div');
 
-            //     // Get the <span> element that closes the modal
-                 var span = document.getElementsByClassName("close")[0];
+        //     li.setAttribute('data-id', doc.id);
+        //     crsName.textContent = doc.data().CRS_NAME;
+        //     taskType.textContent = doc.data().TASK_TYPE;
+        //     dueDate.textContent = doc.data().DUE_DATE;
+        //     taskDetails.textContent = doc.data().TASK_DETAILS;
 
-            //     // When the user clicks the button, open the modal 
-                 btn.onclick = function () {
-                     modal.style.display = "block";
-                 }
+        //     li.appendChild(crsName);
+        //     li.appendChild(taskType);
+        //     li.appendChild(dueDate);
+        //     li.appendChild(taskDetails);
 
-            //     // When the user clicks on <span> (x), close the modal
-                 span.onclick = function () {
-                     modal.style.display = "none";
-                 }
+        //     taskForm.appendChild(li);
+        // }
 
-            //     // When the user clicks anywhere outside of the modal, close it
-                 window.onclick = function (event) {
-                     if (event.target == modal) {
+        // //saving student information
+        // form.addEventListener('submit', (e) =>{
+        //     //e.preventDefault();
+        //     db.collection('assignments').add({
+        //         CRS_NAME: taskForm.crsName.value,
+        //         TASK_TYPE: taskForm.taskType.value,
+        //         DUE_DATE: taskForm.dueDate.value,
+        //         TASK_DETAILS: taskForm.taskDetails.value,
+        //     });
+        //     taskForm.crsName.value = '';
+        //     taskForm.taskType.value = '';
+        //     taskForm.dueDate.value = '';
+        //     taskForm.taskDatails.value = '';
+    });
+    /*
+                //add button and add stuff from textfield
+                 $('#add').click(function () {
+                     console.log("test");
+
+                     // Get the button that opens the modal
+                     var btn = document.getElementById("add");
+
+                //     // Get the <span> element that closes the modal
+                     var span = document.getElementsByClassName("close")[0];
+
+                //     // When the user clicks the button, open the modal 
+                     btn.onclick = function () {
+                         modal.style.display = "block";
+                     }
+
+                //     // When the user clicks on <span> (x), close the modal
+                     span.onclick = function () {
                          modal.style.display = "none";
                      }
-                 }
 
-                 $('.date.col-1').each(function (index) {
-                     console.log($(this).text() === date);
-
-                     if ($(this).text() === date) {
-                         $(this).parent().parent().append(
-                            `<a class=\\"event d-block p-1 pl-2 pr-2 mb-1 rounded text-truncate small bg-success text-white\\" title=\\"Test Event\\">${taskname}</a>`);
-
+                //     // When the user clicks anywhere outside of the modal, close it
+                     window.onclick = function (event) {
+                         if (event.target == modal) {
+                             modal.style.display = "none";
+                         }
                      }
+
+                     $('.date.col-1').each(function (index) {
+                         console.log($(this).text() === date);
+
+                         if ($(this).text() === date) {
+                             $(this).parent().parent().append(
+                                `<a class=\\"event d-block p-1 pl-2 pr-2 mb-1 rounded text-truncate small bg-success text-white\\" title=\\"Test Event\\">${taskname}</a>`);
+
+                         }
+                     });
+
                  });
+    */
+    //delete button delete checked checkbox
+    $('#delete').click(function () {
+        console.log("test");
+        $(".checkbox input:checked").parent().parent().remove();
 
-             });
-*/
-            //delete button delete checked checkbox
-            $('#delete').click(function () {
-                console.log("test");
-                $(".checkbox input:checked").parent().parent().remove();
+    });
 
-            });
+    //add firebase databases
+    var taskForm = document.getElementById("taskForm");
+    addBtn = document.getElementById("addassignment");
+    $(addBtn).click(function (event) {
+        db.collection('tasks').doc().set({
+            CRS_NAME: taskForm.crsName.value,
+            ASSN_NAME: taskForm.assignmentName.value,
+            TASK_TYPE: taskForm.taskType.value,
+            DUE_DATE: taskForm.dueDate.value,
+            TASK_DETAILS: taskForm.taskDetails.value,
+        });
 
-            //add firebase databases
-            var taskForm = document.getElementById("taskForm");
-            addBtn = document.getElementById("addassignment");
-            $(addBtn).click(function(event) {
-                db.collection('tasks').doc().set({
-                    CRS_NAME: taskForm.crsName.value,
-                    ASSN_NAME: taskForm.assignmentName.value,
-                    TASK_TYPE: taskForm.taskType.value,
-                    DUE_DATE: taskForm.dueDate.value,
-                    TASK_DETAILS: taskForm.taskDetails.value,
-                });
-            
-                taskForm.crsName.value = '';
-                taskForm.taskType.value = '';
-                taskForm.dueDate.value = '';
-                taskForm.taskDetails.value = '';
-                taskForm.assignmentName.value = '';
-                $('#taskType').prop('selected', function() {
-                        return this.defaultSelected;
-                    });
-                
-                $('#assignmentName').prop('selected', function() {
-                    return this.defaultSelected;
-                });
-                alert("Added Successfully!");
-                });
-           
-                /*
+        taskForm.crsName.value = '';
+        taskForm.taskType.value = '';
+        taskForm.dueDate.value = '';
+        taskForm.taskDetails.value = '';
+        taskForm.assignmentName.value = '';
+        $('#taskType').prop('selected', function () {
+            return this.defaultSelected;
+        });
+
+        $('#assignmentName').prop('selected', function () {
+            return this.defaultSelected;
+        });
+        alert("Added Successfully!");
+    });
+
+    /*
                  db.collection("tasks").doc().set({
 
                      month: month.valueOf(),
@@ -174,38 +174,53 @@
                  });
             */
 
-            //add firebase databases for adding tasks.
-            
+    //add firebase databases for adding tasks.
 
 
-        logoutBtn = document.getElementById("logout-button");
-        logoutBtn.addEventListener('click', function () {
-          firebase.auth().signOut().then(function() {
+
+    logoutBtn = document.getElementById("logout-button");
+    logoutBtn.addEventListener('click', function () {
+        firebase.auth().signOut().then(function () {
             alert("Signed out successfully!");
             window.location = 'index.html';
             // Sign-out successful.
-          }).catch(function(error) {
+        }).catch(function (error) {
             var errorCode = error.code;
             var errorMessage = error.message;
             alert(errorMessage);
             // An error happened.
-          })});
+        })
+    });
 
-          
-          var query = db.collection('tasks')
-          query.orderBy("date");
-            query.get().then((s) => {
-    
-            var n = 0;
-            s.forEach(function(x){
-               $("#recent-updates-table").append(`
+
+    var query = db.collection('tasks').orderBy("date", "desc");
+    query.get().then((s) => {
+
+        var n = 0;
+        s.forEach(function (x) {
+            n += 1;
+            $("#recent-updates-table").append(`
          
                 
-                  <tr><td>`+ x.data().CRS_NAME +`</td><td>` + x.data().ASSN_NAME +`</td><td>` + x.data().DUE_DATE + `</td></tr>
+                  <tr><td>` + x.data().course + `</td><td>` + x.data().taskname + `</td><td>` + x.data().date + '/' + x.data().month + `</td></tr>
          `);
-         
-               n += 1;
+            console.log(n);
+            if (n <= 20) {
+                $("#cb").append(`<div class="checkbox"<label>` + n + " " + `<input type="checkbox" value=""><span>Course:      
+                   ` + x.data().course + `, ` + x.data().taskname + ` DATE: ` + x.data().date + `</span></label>
+                   </div> 
+         `);
+            }
+
+            $('.date.col-1').each(function (index) {
+                console.log($(this).text() === x.data().date);
+
+                if ($(this).text() === x.data().date) {
+                    $(this).parent().parent().append(
+                        `<a class="event d-block p-1 pl-2 pr-2 mb-1 rounded text-truncate small bg-primary text-white"
+                    title="Test">` + x.data().taskname + " " + x.data().course + `</a>`);
+
+                }
             });
-         });
-
-
+        });
+    });
